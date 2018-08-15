402c3f72bc2a54f753ac4014c0cf5bf93eb1a3cb
Amith Mohanan <amithm7@gmail.com>
2018-08-16 03:21:13 +0530

    refactor(minor): better variable names

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,12 +1,11 @@
 var extensionID = browser.i18n.getMessage("@@extension_id");	// Extension id
 
 window.onload = function() {
-
 	// Extract the current tab open which extension popup is opened
-	browser.tabs.query({ currentWindow: true, active: true }).then(function(tabs){
-		var tab = tabs[0];
-		console.log(tab);
-		showMenu(tab);
+	browser.tabs.query({ currentWindow: true, active: true }).then(function(input) {
+		var currentTab = input[0];
+		console.log(currentTab);
+		showMenu(currentTab);
 	});
 
 	// Open collection of links
@@ -21,32 +20,27 @@ window.onload = function() {
 };
 
 // Show popup menu
-function showMenu(tab) {
-	// A link's data
-	var link = {
-		title: tab.title,
-		url: tab.url,
-		favIconUrl: tab.favIconUrl,
+function showMenu(currentTab) {
+	// A tab's data to store
+	var tab = {
+		title: currentTab.title,
+		url: currentTab.url,
+		favIconUrl: currentTab.favIconUrl,
 
-		name: getTitleName(tab.title, tab.url),
+		name: getTitleName(currentTab.title, currentTab.url),
 		tags: {}
 	};
-
-	// Extract Actual title - To do
-	function getTitleName(title, url) {
-		return title;
-	}
-
+	
 	// Display form for input
 	var title = document.getElementsByClassName('link-title')[0];
-	title.value = link.title;
+	title.value = tab.title;
 	var url = document.getElementsByClassName('link-url')[0];
-	url.value = link.url;
-
+	url.value = tab.url;
+	
 	// Get tags data
 	var tag = document.getElementsByClassName('tag-name')[0].getElementsByTagName('input')[0];
 	var tagVal = document.getElementsByClassName('tag-value')[0].getElementsByTagName('input')[0];
-
+	
 	tagsP = browser.storage.local.get('tags');
 	tagsP.then(function(input) {
 		$('.tag-name input').autocomplete({
@@ -54,7 +48,7 @@ function showMenu(tab) {
 			autoFocus: true
 		});
 	});
-
+	
 	tagVal.addEventListener('focus', function(){
 		if (tag.value !== "") {
 			var tagIn = tag.value;
@@ -69,64 +63,64 @@ function showMenu(tab) {
 			});
 		}
 	});
-
+	
 	// To add multiple tags
 	document.getElementsByClassName('tag-name')[0].getElementsByTagName('button')[0].addEventListener('click', function() {
 		// Clear input fields
-		addTag(link, tag, tagVal);
+		addTag(tab, tag, tagVal);
 		tag.value = "";
 		tagVal.value = "";
 	});
-
+	
 	// Multiple tag values
 	document.getElementsByClassName('tag-value')[0].getElementsByTagName('button')[0].addEventListener('click', function() {
-		addTag(link, tag, tagVal);
+		addTag(tab, tag, tagVal);
 		tagVal.value = "";
 	});
-
+	
 	// Form submit click event
 	document.getElementsByClassName('form-submit')[0].getElementsByTagName('button')[0].addEventListener('click', function() {
-		link.title = title.value;
-		link.url = url.value;
-
-		addTag(link, tag, tagVal);
-
-		console.log(link);
-		saveTab(link);
+		tab.title = title.value;
+		tab.url = url.value;
+		
+		addTag(tab, tag, tagVal);
+		
+		console.log(tab);
+		saveTab(tab);
 	});
 }
 
 // Add tag data to link object
-function addTag(link, tag, tagVal) {
+function addTag(tab, tag, tagVal) {
 	var key = tag.value;
 	var value = tagVal.value;
 	if (tag.value !== ""){
-		if(link.tags[key] != undefined && link.tags[key].length > 0) {
+		if(tab.tags[key] != undefined && tab.tags[key].length > 0) {
 			if (value !== "")
-				link.tags[key].push(value);
+			tab.tags[key].push(value);
 		} else {
-			link.tags[key] = [];
+			tab.tags[key] = [];
 			if (value !== "")
-				link.tags[key].push(value);
+			tab.tags[key].push(value);
 		}
 	}
 }
 
 // Save Tab data to memory
-function saveTab(link) {
+function saveTab(tab) {
 	var key;
 	var keys = [];
-
+	
 	// Tab ids
 	keysP = browser.storage.local.get('keys');
 	keysP.then(gotKeys);
-
+	
 	function gotKeys(input) {		// async
 		console.log(input);
-
+		
 		var storageUpdated;
-		var linkTags = link.tags;
-
+		var linkTags = tab.tags;
+		
 		if (input.keys != undefined && input.keys.length > 0) {
 			keys = input.keys;
 			key = keys[keys.length - 1] + 1;
@@ -136,63 +130,63 @@ function saveTab(link) {
 		keys.push(key);
 		// Update tab id's array
 		browser.storage.local.set({'keys': keys});
-
+		
 		// Add tab data to store obejct
 		var store = {};
-		store[key] = link;
-
+		store[key] = tab;
+		
 		// If no tags exist
 		if (Object.keys(linkTags).length == 0) {
 			// Update storage
 			storageUpdated = browser.storage.local.set(store);
-
+			
 			storageUpdated.then(function(){
 				disableSaveBtn();
-
+				
 				// reload collection page after adding link
 				reloadCollection();
 			});
-
+			
 		} else {
 			// Add tags array to store object
 			var tagsArr = [];
 			tagsP = browser.storage.local.get('tags');
 			tagsP.then(gotTags);
 		}
-
+		
 		function gotTags(input) {
 			if (input.tags != undefined && input.tags.length > 0) {
 				tagsArr = input.tags;
 			}
-			for (var tag in link.tags) {
+			for (var tag in tab.tags) {
 				tagsArr.push(tag);
 			}
-
+			
 			store.tags = Array.from(new Set(tagsArr));
-
+			
 			// Store tag values array to store object
-			for (tag in link.tags) {
+			for (tag in tab.tags) {
 				storeTag(tag);
 			}
-
+			
 			function storeTag(tag) {
 				tagVP = browser.storage.local.get(tag);
 				tagVP.then(gotTagV);
-
+				
 				function gotTagV(input) {
 					var tagVArr = [];
 					if (input[tag] != undefined && input[tag].length > 0) {
 						tagVArr = input[tag];
 					}
-					tagVArr.push.apply(tagVArr, link.tags[tag]);
-
+					tagVArr.push.apply(tagVArr, tab.tags[tag]);
+					
 					store[tag] = Array.from(new Set(tagVArr));
-
+					
 					// Update storage
 					storageUpdated = browser.storage.local.set(store);
 					storageUpdated.then(function () {
 						disableSaveBtn();
-
+						
 						// reload collection page after adding link
 						reloadCollection();
 					});
@@ -200,6 +194,11 @@ function saveTab(link) {
 			}
 		}
 	}
+}
+
+// Extract Actual title - To do
+function getTitleName(title, url) {
+	return title;
 }
 
 function disableSaveBtn() {
