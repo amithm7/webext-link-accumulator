d3cc33caa07f1d4c51c9a5879a5e9cff1b246d60
Amith Mohanan <amithm7@gmail.com>
2018-08-17 21:35:52 +0530

    feat(Organise): Collection: Ablility to sort by date

--- src/collection/links.js
+++ src/collection/links.js
@@ -1,47 +1,90 @@
 render();
 
-var list = document.getElementById('links');
+// Set background color of an element
+function setBackground(ele, color) {
+	ele.style.background = color;
+}
+
+var collectionSection = document.querySelector('.collection');
+
+function displayList(store, tabIDList) {
+	var list = document.createElement('ul');
+	collectionSection.appendChild(list);
+
+	// Iterate over stored links and display them
+	for (i = 0; i < tabIDList.length; i++) {
+		var tab = store[tabIDList[i]];
+		console.log(tab);
+
+		var link = document.createElement('a');
+		link.setAttribute('href', tab.url);
+		link.innerHTML = tab.title;
+
+		var li = document.createElement('li');
+
+		list.appendChild(li).appendChild(link);
+
+		// Iterate over all tags
+		for (var key in tab.tags) {
+			var tag = document.createElement('span');
+			tag.setAttribute('class', 'tooltip tag');
+			tag.innerHTML = key;
+
+			rcolor = getColor();
+			setBackground(tag, rcolor);
+
+			var tagvalue = document.createElement('span');
+			tagvalue.setAttribute('class', 'tooltiptext tag-value');
+			tagvalue.innerHTML = tab.tags[key];
+
+			li.appendChild(tag).appendChild(tagvalue);
+		}
+	}
+}
+
+function displayNoSort(store) {
+	collectionSection.innerHTML = "";
+
+	var tabIDList = store.tabIDList;
+
+	displayList(store, tabIDList);
+}
+
+function displayByDate(store) {
+	collectionSection.innerHTML = "";
+
+	Object.keys(store.tabsOnDate).forEach(function(date) {
+		var dateHeader = document.createElement('h3');
+		dateHeader.innerHTML = date;
+		collectionSection.appendChild(dateHeader);
+
+		var tabIDList = store.tabsOnDate[date];
+
+		displayList(store, tabIDList);
+	});
+}
 
 function render() {
 	browser.storage.local.get().then(onGot);
 
 	function onGot(store) {
 		console.log(store);
-		// Iterate over stored links and display them
-		for (i = 0; i < store.tabIDList.length; i++) {
-			var tab = store[store.tabIDList[i]];
-			console.log(tab);
 
-			var link = document.createElement('a');
-			link.setAttribute('href', tab.url);
-			link.innerHTML = tab.title;
-			
-			var li = document.createElement('li');
-			
-			list.appendChild(li).appendChild(link);
+		var sorting = document.querySelector('.list-sorting').sortBy;
 
-			// Iterate over all tags
-			for(var key in tab.tags) {
-				var tag = document.createElement('span');
-				tag.setAttribute('class', 'tooltip tag');
-				tag.innerHTML = key;
+		for(i = 0; i < 2; i++){
+			sorting[i].addEventListener('click', sortBy);
+		}
 
-				rcolor = getColor();
-				setBackground(tag, rcolor);
-
-				var tagvalue = document.createElement('span');
-				tagvalue.setAttribute('class', 'tooltiptext tag-value');
-				tagvalue.innerHTML = tab.tags[key];
-
-				li.appendChild(tag).appendChild(tagvalue);
+		function sortBy() {
+			if (sorting.value == "none") {
+				displayNoSort(store);
+			} else if (sorting.value == "byDate"){
+				displayByDate(store);
 			}
 		}
+		sortBy();
 	}
-}
-
-// Set background color of an element
-function setBackground(ele, color) {
-	ele.style.background = color;
 }
 
 // Delete all links
